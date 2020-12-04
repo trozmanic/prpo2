8b3957cfabace39224a66ce3479afac45b9f9444
te2445 <36800093+te2445@users.noreply.github.com>
2020-12-04 23:55:21 +0100

    final?

--- storitve/src/main/java/si/fri/prpo/skupina57/storitve/intercceptorji/ValidacijaDatumaInterceptor.java
+++ storitve/src/main/java/si/fri/prpo/skupina57/storitve/intercceptorji/ValidacijaDatumaInterceptor.java
@@ -22,10 +22,30 @@ public class ValidacijaDatumaInterceptor {
 
         log.info("Pognan validator datuma");
 
-        log.info(context.getParameters()[0].toString());
-        log.info(context.getParameters()[1].toString());
+        //log.info(context.getParameters()[0].toString());
+        //log.info(context.getParameters()[1].toString());
 
+        //[0] ni bil dto, je bil [1], verjetno je 0 id, 1 pa dto ? mogoce ker so tako po vrsti v metodi?
+        //for loop
+        for (int i = 0; i < context.getParameters().length; i++) {
+            if (context.getParameters()[i] instanceof GovorilnaUraDto) {
+                GovorilnaUraDto govorilnaUraDto = (GovorilnaUraDto) context.getParameters()[i];
 
+                log.info("Pognan validator datuma najdel DTO");
+
+                if(govorilnaUraDto.getDatum() != null){
+                    Date datum = govorilnaUraDto.getDatum();
+                    Date dateNow = new Date();
+                    if(datum.compareTo(dateNow) < 0){
+                        String message = "Datum govorilne ure ni ustrezen";
+                        log.severe(message);
+                        throw new DatumPreteklostIzjema(message);
+                    }
+
+                }
+            }
+        }
+        /*
         if(context.getParameters().length == 1 && context.getParameters()[0] instanceof GovorilnaUraDto){
             GovorilnaUraDto govorilnaUraDto = (GovorilnaUraDto) context.getParameters()[0];
 
@@ -42,6 +62,7 @@ public class ValidacijaDatumaInterceptor {
 
             }
         }
+        */
         return context.proceed();
     }
 }
